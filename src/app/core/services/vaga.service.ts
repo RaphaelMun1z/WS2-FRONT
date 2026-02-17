a82b49ae81a3a54ff240aa44903f4ab0f46e1d63
Raphael Muniz <raphaelmunizvarela@gmail.com>
2026-02-17 02:02:06 -0300

    ok

--- src/app/core/services/vaga.service.ts
+++ src/app/core/services/vaga.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http'
 import { Observable, of, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Vaga, FiltrosBusca, Page } from '../models/vaga.models';
+import { environment } from '../../../environments/environment';
 
 export type ApiErrorTipo = 'SERVIDOR_OFFLINE' | 'ERRO_SERVIDOR' | 'ERRO_DESCONHECIDO';
 
@@ -15,7 +16,7 @@ export interface ApiError {
 @Injectable({ providedIn: 'root' })
 export class VagaService {
   private http = inject(HttpClient);
-  private apiUrl = 'http://localhost:8080/api/vagas';
+  private apiUrl = `${environment.apiUrl}/vagas`;
 
   listarVagas(filtros: FiltrosBusca, page: number = 0, size: number = 12): Observable<Page<Vaga>> {
     let params = new HttpParams()
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://ws2-back.onrender.com/api',
+};
